c131c6fc232eb9c7ad4d28a0410030777bcad80b
FrecHHhh <ultima01234567@gmail.com>
2025-11-16 21:01:29 -0300

    Refactor Titanic analysis with new visualizations
    Updated analysis and visualizations for Titanic dataset.

--- uu.py
+++ uu.py
@@ -5,15 +5,33 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("database_titanic.csv")
 
-st.write("CONTROL3")
+st.header("Control3")
+st.write("En esta pagina reunimos datos sobre las personas que abordaron el titanic, los cuales veremos representados tanto de forma grafica y tabulados.")
 
 with st.sidebar:
     div = st.slider('Número de bins:', 0, 10, 2)
     st.write("Bins=", div)
-    
 
-st.header("Análisis de supervivientes")
-st.write("En en grafico a continuacion podemos ver un grafico comparativo de sobrevivientes y no sobrevivientes del Titanic.")
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
+ax[0].hist(df["Age"], bins=div)
+ax[0].set_title("Edad")
+ax[0].set_ylabel("Frecuencia")
+ax[0].set_xlabel("Histograma de edades")
+
+df_male = df[df["Sex"] == "male"]
+cant_male = len(df_male)
+
+df_female = df[df["Sex"] == "female"]
+cant_female = len(df_female)
+
+ax[1].bar(["Masculino", "Femenino"], [cant_male, cant_female], color = "red")
+ax[1].set_title("Sexo")
+ax[1].set_ylabel("Cantidad")
+ax[1].set_xlabel('Distribucion de hombres y mujeres')
+
+datos_sexo = df.groupby('Sex')['Survived'].value_counts().unstack()
+datos_sexo = datos_sexo.rename(columns={0: 'No Sobrevivió', 1: 'Sobrevivió'})
+
 
 datos_sexo = df.groupby('Sex')['Survived'].value_counts().unstack()
 datos_sexo = datos_sexo.rename(columns={0: 'No Sobrevivió', 1: 'Sobrevivió'})
@@ -31,8 +49,13 @@ datos_sexo.plot(kind='bar',
 ax_sexo.set_title('Supervivencia por Sexo')
 ax_sexo.set_ylabel('Número de Pasajeros')
 ax_sexo.set_xlabel('Sexo')
-ax_sexo.tick_params(axis='x', rotation=0) 
-ax_sexo.legend(title='Destino')
 
+st.header("Analisis de pasajeros")
+st.pyplot(fig)
 
+st.header("Analisis de supervivientes")
+st.write("En el grafico a continuacion podemos ver la cantidad de personas que sobrevivieron y las que no en el titanic, ademas de su sexo.")
 st.pyplot(fig_sexo)
+
+st.header("Datos")
+st.table(df.head())
